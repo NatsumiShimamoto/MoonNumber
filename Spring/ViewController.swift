56e20bb1c8e95487fc26fb16cf3736c4452b5c8d
NatsumiShimamoto <nshimamoto725@gmail.com>
2016-03-23 10:25:05 +0900

    Initial Commit

---
+++ Spring/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spring
+//
+//  Created by 嶋本夏海 on 2016/03/23.
+//  Copyright © 2016年 嶋本夏海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
